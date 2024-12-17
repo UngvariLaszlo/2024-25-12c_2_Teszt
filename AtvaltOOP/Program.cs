d61959def7c3335e1e192b38921f2dcd5b1ad960
ungva <ungva@LACKO-PC>
2024-12-17 20:52:35 +0100

    Beadandó

---
+++ AtvaltOOP/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Atvalt
+{
+    public string Szam { get; set; }
+
+    // Konstruktor
+    public Atvalt(string szam)
+    {
+        Szam = szam;
+    }
+
+    // Átváltás bináris és tizes számrendszer között
+    public string AtvaltSzam()
+    {
+        if (IsBin(Szam))
+        {
+            // Bináris számot tizesre konvertálunk
+            return Convert.ToInt32(Szam, 2).ToString();
+        }
+        else if (IsDecimal(Szam))
+        {
+            // Tizes számot binárisra konvertálunk
+            return Convert.ToString(int.Parse(Szam), 2);
+        }
+        else
+        {
+            // Hibás adatmegadás
+            throw new ArgumentException("Hibás adat: A szám nem bináris vagy tizes szám.");
+        }
+    }
+
+    // Bináris szám ellenőrzése
+    private bool IsBin(string szam)
+    {
+        foreach (var c in szam)
+        {
+            if (c != '0' && c != '1')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Tizes szám ellenőrzése
+    private bool IsDecimal(string szam)
+    {
+        return int.TryParse(szam, out _);
+    }
+
+    // Eredmény lekérése sztringként
+    public string Eredmeny()
+    {
+        return AtvaltSzam();
+    }
+}
---
+++ AtvaltOOP_test/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+[TestFixture]
+public class AtvaltTests
+{
+    [Test]
+    public void Test_BinToDecimal()
+    {
+        // Bináris szám átváltása tizes számrendszerbe
+        var atvalt = new Atvalt("1011");
+        Assert.AreEqual("11", atvalt.Eredmeny());
+    }
+
+    [Test]
+    public void Test_DecimalToBin()
+    {
+        // Tizes szám átváltása bináris számrendszerbe
+        var atvalt = new Atvalt("11");
+        Assert.AreEqual("1011", atvalt.Eredmeny());
+    }
+
+    [Test]
+    public void Test_InvalidInput()
+    {
+        // Hibás adatbevitel (nem bináris és nem tizes szám)
+
+        // Az Assert.Throws metódus használata a megfelelő szintaxissal
+        var ex = Assert.Throws<FormatException>(() => new Atvalt("12").Eredmeny());
+        Assert.That(ex.Message, Is.EqualTo("Hibás adat: A szám nem bináris vagy tizes szám."));
+
+        ex = Assert.Throws<FormatException>(() => new Atvalt("abc").Eredmeny());
+        Assert.That(ex.Message, Is.EqualTo("Hibás adat: A szám nem bináris vagy tizes szám."));
+
+        ex = Assert.Throws<FormatException>(() => new Atvalt("1012").Eredmeny());
+        Assert.That(ex.Message, Is.EqualTo("Hibás adat: A szám nem bináris vagy tizes szám."));
+    }
+}
